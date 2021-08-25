alexandru-bagu/FreeRDP

allow a way to have different configs for each session

server/proxy/pf_config.h
@@ -94,7 +94,8 @@ extern "C"
 };
 #endif
 
-proxyConfig* pf_server_config_load(const char* path);
+proxyConfig* pf_server_config_load_file(const char* path);
+proxyConfig* pf_server_config_load_buffer(const char* path);
 void pf_server_config_print(proxyConfig* config);
 void pf_server_config_free(proxyConfig* config);
 

whopeprgerte/FreeRDP-1

server/proxy/pf_config.h
@@ -94,7 +94,8 @@ extern "C"
 };
 #endif
 
-proxyConfig* pf_server_config_load(const char* path);
+proxyConfig* pf_server_config_load_file(const char* path);
+proxyConfig* pf_server_config_load_buffer(const char* buffer);
 void pf_server_config_print(proxyConfig* config);
 void pf_server_config_free(proxyConfig* config);
 